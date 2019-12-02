02f64480aa3d4ac8a439ca29007241ae2fd24f31
Lukas <lukasHD@users.noreply.github.com>
2019-12-02 22:08:25 +0100

    basic runner works and test is completed

--- Day2/day2.py
+++ Day2/day2.py
@@ -5,15 +5,37 @@
 #
 # 
 
+
+
+
+
 def readOpCode(op):
     if op == 1:
         print("add")
-    else if op == 2:
+        return 
+    elif op == 2:
         print("mul")
-    else if op == 99:
+    elif op == 99:
         print("break")
 
 intInput = [1,9,10,3,2,3,11,0,99,30,40,50]
 
-for idx, val in enumerate(intInput):
-  print("index is %d and value is %s" % (idx, val))
+def runIntcode(intInput):
+    for idx, val in enumerate(intInput):
+        #print("index is %d and value is %s" % (idx, val))
+        print("Index: {}".format(idx))
+        print(intInput)
+        #readOpCode(val)
+        if val == 1:
+            print("add({}, {}, {})".format(intInput[idx+1], intInput[idx+2], intInput[idx+3]))
+            print("L[{}] = {} + {} = {}".format(intInput[idx+3], intInput[intInput[idx+1]], intInput[intInput[idx+2]], intInput[intInput[idx+1]] + intInput[intInput[idx+2]]))
+            intInput[intInput[idx+3]] = intInput[intInput[idx+1]] + intInput[intInput[idx+2]]
+        elif val == 2:
+            print("mul({}, {}, {})".format(intInput[idx+1], intInput[idx+2], intInput[idx+3]))
+            print("L[{}] = {} * {} = {}".format(intInput[idx+3], intInput[intInput[idx+1]], intInput[intInput[idx+2]], intInput[intInput[idx+1]] * intInput[intInput[idx+2]]))
+            intInput[intInput[idx+3]] = intInput[intInput[idx+1]] * intInput[intInput[idx+2]]
+        elif val == 99:
+            print("break")
+            return(intInput)
+            break
+
--- Day2/run_tests.py
+++ Day2/run_tests.py
@@ -4,45 +4,10 @@ import unittest
 # run tests like this `python -m unittest day1.py -v`
 
 
-class TestClass(unittest.TestCase):
+class TestRunner(unittest.TestCase):
     
-    def test_day1_1_1(self):
-        a = day1.module(12)
-        self.assertEqual(a.requiredFuel(), 2)
-    def test_day1_1_2(self):
-        a = day1.module(14)
-        self.assertEqual(a.requiredFuel(), 2)
-    def test_day1_1_3(self):
-        a = day1.module(1969)
-        self.assertEqual(a.requiredFuel(), 654)
-    def test_day1_1_4(self):
-        a = day1.module(100756)
-        self.assertEqual(a.requiredFuel(), 33583)
-
-
-class TestMethods(unittest.TestCase):
+    def test_day2_1_1(self):
+        self.assertEqual(day2.runIntcode([1,9,10,3,2,3,11,0,99,30,40,50]), [3500,9,10,70,2,3,11,0,99,30,40,50])
     
-    def test_day1_1_5(self):
-        self.assertEqual(day1.calcFuel(12), 2)
-    def test_day1_1_6(self):
-        self.assertEqual(day1.calcFuel(14), 2)
-    def test_day1_1_7(self):
-        self.assertEqual(day1.calcFuel(1969), 654)
-    def test_day1_1_8(self):
-        self.assertEqual(day1.calcFuel(100756), 33583)
-
-class TestFullFuel(unittest.TestCase):
-
-    def test_day1_2_1(self):
-        self.assertEqual(day1.calcFullFuel(14), 2)
-    def test_day1_2_2(self):
-        self.assertEqual(day1.calcFullFuel(1969), 966)
-    def test_day1_2_3(self):
-        self.assertEqual(day1.calcFullFuel(100756),50346)
-    def test_negative_full_fuel(self):
-        self.assertEqual(day1.calcFullFuel(-10),0)
-    def test_zero_full_fuel(self):
-        self.assertEqual(day1.calcFullFuel(0),0)
-
 if __name__ == '__main__':
     unittest.main()
