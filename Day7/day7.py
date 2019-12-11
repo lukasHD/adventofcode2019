71c88c8a7523018200bd4fa7ab8d7f05a8f856ad
Lukas <lukasHD@users.noreply.github.com>
2019-12-11 22:29:57 +0100

    day7 part 1 and tests for part 2

--- Day7/day7.py
+++ Day7/day7.py
@@ -3,6 +3,8 @@
 #
 # --- Day 5: Sunny with a Chance of Asteroids ---
 # 
+import itertools
+from unittest.mock import patch
 
 def loadintCode(fname='input'):
     with open(fname, 'r') as f:
@@ -33,18 +35,22 @@ def MUL(vals):
     a,b = vals
     return a*b
 
-def INP(vals, sim=None):
-    if sim is not None: 
-        print("simulate input value: {}".format(sim))
-        return sim
+def INP(vals, interactive=True, value=0):
+    if not interactive: 
+        #print("simulate input value: {}".format(value))
+        return value
     else: 
         print("Enter input: ")
-        sim = int(input())
-        return sim
+        value = int(input())
+        return value
 
-def OUT(vals):
+def OUT(vals, interactive=True):
     if len(vals) != 1: raise TypeError
-    print("Output is: {}".format(vals[0]))
+    if not interactive:
+        return  vals[0]
+    else:
+        print("Output is: {}".format(vals[0]))
+
 
 def JPT(vals):
     if vals[0] != 0: 
@@ -94,9 +100,10 @@ def decode(val):
     else:
         return None
 
-def runCode(intInput, debug=False):
+def runCode(intInput, debug=False, interactive=True, _in=None):
     ignore = 0
     idx = 0
+    output = []
     #for idx, val in enumerate(intInput):
     while(idx <= len(intInput)):
         val = intInput[idx]
@@ -109,7 +116,10 @@ def runCode(intInput, debug=False):
         op, numVar, writes, jumps = decode(cmd)
         if op == END:
             op()
-            return intInput
+            if interactive == True:
+                return intInput
+            else:
+                return output
         modes = val//100
         mod= []
         while (modes > 0):
@@ -132,31 +142,75 @@ def runCode(intInput, debug=False):
                 raise RuntimeError
         if writes:
             # an opcode that writes to last parameter
-            intInput[intInput[idx+numVar]] = op(vars[:-1])
+            if op == INP and interactive == False:
+                intInput[intInput[idx+numVar]] = op(vars[:-1], interactive=False, value=_in.pop(0))
+            else:
+                intInput[intInput[idx+numVar]] = op(vars[:-1])
         elif jumps:
-            print("JUMP")
+            #print("JUMP")
             if op(vars[:-1]):
                 idx = vars[-1]
                 continue
         else:
-            op(vars)
+            if op == OUT and interactive == False:
+                output.append(op(vars, interactive=False))
+            else:
+                op(vars)
         ignore = numVar
         idx += 1
 
-
 # day7
+def run_Prog_non_interactive(_prog, _in):
+    out = runCode(_prog, debug=False, interactive=False, _in=_in)
+    return out[-1]
+
 
 def optimize(prog):
     maxThrust = 0
     phaseSeq = []
-    for perm in permutations:
-        # bla bla bla 
-        print(perm)
-    return maxThrust, phaseSeq 
+    resultList = []
+    for perm in list(itertools.permutations([0, 1, 2, 3, 4])): 
+        #print(perm)
+        out1 = run_Prog_non_interactive(prog, [perm[0], 0])
+        out2 = run_Prog_non_interactive(prog, [perm[1], out1])
+        out3 = run_Prog_non_interactive(prog, [perm[2], out2])
+        out4 = run_Prog_non_interactive(prog, [perm[3], out3])
+        out5 = run_Prog_non_interactive(prog, [perm[4], out4])
+        resultList.append((perm, out5))
+    #print(resultList)
+    bla = max(resultList, key=lambda x: x[1])
+    #print(bla)
+    phaseSeq, maxThrust = bla
+    print(maxThrust)
+    print(phaseSeq)
+    return maxThrust, list(phaseSeq) 
+
+def optimize2(prog):
+    maxThrust = 0
+    phaseSeq = []
+    resultList = []
+    for perm in list(itertools.permutations([0, 1, 2, 3, 4])): 
+        #print(perm)
+        out1 = run_Prog_non_interactive(prog, [perm[0], 0])
+        out2 = run_Prog_non_interactive(prog, [perm[1], out1])
+        out3 = run_Prog_non_interactive(prog, [perm[2], out2])
+        out4 = run_Prog_non_interactive(prog, [perm[3], out3])
+        out5 = run_Prog_non_interactive(prog, [perm[4], out4])
+        resultList.append((perm, out5))
+    #print(resultList)
+    bla = max(resultList, key=lambda x: x[1])
+    #print(bla)
+    phaseSeq, maxThrust = bla
+    print(maxThrust)
+    print(phaseSeq)
+    return maxThrust, list(phaseSeq) 
 
 
 def runPartOne():
+    #optimize([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
     prog = loadintCode('input_day7')
+    optimize(prog)
+    
 
 if __name__ == '__main__':
     runPartOne()
--- Day7/run_tests.py
+++ Day7/run_tests.py
@@ -5,9 +5,11 @@ import re
 
 from unittest.mock import patch
 
-progA = """3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"""
-progB = """3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"""
-progC = """3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"""
+progA = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
+progB = [3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0]
+progC = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
+progD = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+progE = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
 
 
 class TestRunner(unittest.TestCase):
@@ -330,6 +332,15 @@ class TestRunner(unittest.TestCase):
         self.assertEqual(maxThrust, 65210)
         self.assertEqual(phaseSeq, [1,0,4,3,2])
     
+    def test_z_day7_2_1(self):
+        maxThrust, phaseSeq = day7.optimize2(progD)
+        self.assertEqual(maxThrust, 139629729)
+        self.assertEqual(phaseSeq, [9,8,7,6,5])
+
+    def test_z_day7_2_1(self):
+        maxThrust, phaseSeq = day7.optimize2(progE)
+        self.assertEqual(maxThrust, 18216)
+        self.assertEqual(phaseSeq, [9,7,8,5,6])
     # def test_day6_2_1(self):
     #     self.assertEqual(day6.getNumberOfTransfers("test_input_2"), 4)
 
