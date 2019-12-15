3f637fd5bf35a3529e549a4abd245ff7424b6a49
Lukas <lukasHD@users.noreply.github.com>
2019-12-15 21:16:23 +0100

    day9 skeleton

---
+++ Day9/day9.py
@@ -0,0 +1,355 @@
+# 
+# --- Day 2: 1202 Program Alarm ---
+#
+# --- Day 5: Sunny with a Chance of Asteroids ---
+#
+# --- Day 7: Amplification Circuit ---
+#
+# --- Day 9: Sensor Boost ---
+# 
+import itertools
+from unittest.mock import patch
+import queue
+
+def loadintCode(fname='input'):
+    with open(fname, 'r') as f:
+        l = list(f.read().split(','))
+        p = [int(x) for x in l]
+        return p
+
+def printIndexValue(L, pos=0):
+    longest = len(str(max(L)))
+    print("[",end='')
+    for idx, val in enumerate(L):
+        print("{:{width}d},".format(val, width=longest+1),end='')
+    print("]")
+    indices = list(range(len(L)))
+    indices[pos] = "^"*(longest+1)
+    print("(",end='')
+    for idx in indices:
+        print("{:^{width}s},".format(str(idx), width=longest+1),end='')
+    print(")")
+
+def ADD(vals):
+    if len(vals) != 2: raise TypeError
+    a,b = vals
+    return a+b
+
+def MUL(vals):
+    if len(vals) != 2: raise TypeError
+    a,b = vals
+    return a*b
+
+def INP(vals, interactive=True, value=0):
+    if not interactive: 
+        #print("simulate input value: {}".format(value))
+        return value
+    else: 
+        print("Enter input: ")
+        value = int(input())
+        return value
+
+def OUT(vals, interactive=True):
+    if len(vals) != 1: raise TypeError
+    if not interactive:
+        return  vals[0]
+    else:
+        print("Output is: {}".format(vals[0]))
+
+
+def JPT(vals):
+    if vals[0] != 0: 
+        return True
+    else:
+        return False
+
+def JPF(vals):
+    if vals[0] == 0:
+        return True
+    else: 
+        return False
+
+def LES(vals):
+    if vals[0] < vals[1]:
+        return 1
+    else:
+        return 0
+
+def EQL(vals):
+    if vals[0] == vals[1]:
+        return 1
+    else:
+        return 0
+
+def END():
+    #print("END")
+    return "END"
+
+instrSet = {
+    # code: (FUNCTION, #ofParams, Outputs, jumps)
+    1: (ADD, 3, True, False),
+    2: (MUL, 3, True, False),
+    3: (INP, 1, True, False),
+    4: (OUT, 1, False, False),
+    5: (JPT, 2, False, True),
+    6: (JPF, 2, False, True),
+    7: (LES, 3, True, False),
+    8: (EQL, 3, True, False),
+    99: (END, 0, False, True)
+}
+
+def decode(val):
+    if val in instrSet.keys():
+        # valid op code
+        return instrSet[val]
+    else:
+        return None
+
+def runCode(intInput, debug=False, interactive=True, _in=None):
+    ignore = 0
+    idx = 0
+    output = []
+    #for idx, val in enumerate(intInput):
+    while(idx <= len(intInput)):
+        val = intInput[idx]
+        if ignore > 0:
+            ignore -= 1
+            idx += 1
+            continue
+        if debug: printIndexValue(intInput, idx)
+        cmd = val%100
+        op, numVar, writes, jumps = decode(cmd)
+        if op == END:
+            op()
+            if interactive == True:
+                return intInput
+            else:
+                return output
+        modes = val//100
+        mod= []
+        while (modes > 0):
+            tmp = modes%10
+            if tmp not in [0, 1]: raise TypeError
+            mod.append(tmp)
+            modes = modes//10
+        # now run op(vars)
+        vars = []
+        for i in range(numVar):
+            try:
+                m = mod[i]
+            except IndexError:
+                m = 0
+            if m == 0:
+                vars.append(intInput[intInput[idx+1+i]])
+            elif m == 1:
+                vars.append(intInput[idx+1+i])
+            else: 
+                raise RuntimeError
+        if writes:
+            # an opcode that writes to last parameter
+            if op == INP and interactive == False:
+                intInput[intInput[idx+numVar]] = op(vars[:-1], interactive=False, value=_in.pop(0))
+            else:
+                intInput[intInput[idx+numVar]] = op(vars[:-1])
+        elif jumps:
+            #print("JUMP")
+            if op(vars[:-1]):
+                idx = vars[-1]
+                continue
+        else:
+            if op == OUT and interactive == False:
+                output.append(op(vars, interactive=False))
+            else:
+                op(vars)
+        ignore = numVar
+        idx += 1
+
+# day7
+def run_Prog_non_interactive(_prog, _in):
+    out = runCode(_prog, debug=False, interactive=False, _in=_in)
+    return out[-1]
+
+
+def optimize(prog):
+    maxThrust = 0
+    phaseSeq = []
+    resultList = []
+    for perm in list(itertools.permutations([0, 1, 2, 3, 4])): 
+        #print(perm)
+        out1 = run_Prog_non_interactive(prog, [perm[0], 0])
+        out2 = run_Prog_non_interactive(prog, [perm[1], out1])
+        out3 = run_Prog_non_interactive(prog, [perm[2], out2])
+        out4 = run_Prog_non_interactive(prog, [perm[3], out3])
+        out5 = run_Prog_non_interactive(prog, [perm[4], out4])
+        resultList.append((perm, out5))
+    #print(resultList)
+    bla = max(resultList, key=lambda x: x[1])
+    #print(bla)
+    phaseSeq, maxThrust = bla
+    print(maxThrust)
+    print(phaseSeq)
+    return maxThrust, list(phaseSeq) 
+
+class Amp():
+    def __init__(self, prog):
+        self.prog          = prog.copy()
+        self.inp           = queue.Queue()
+        self.output        = 0
+        self.input_counter = 0
+        self.halted        = False
+        self.finisehd      = False
+        self.idx           = 0
+
+    def run(self, debug=False, interactive=False):
+        ignore = 0
+        output = []
+        while not self.finisehd:
+            val = self.prog[self.idx]
+            if ignore > 0:
+                ignore -= 1
+                self.idx += 1
+                continue
+            #if debug: printIndexValue(self.prog, self.idx)
+            cmd = val%100
+            op, numVar, writes, jumps = decode(cmd)
+            if op == END:
+                op()
+                #print("END END END")
+                if debug: print(output)
+                self.finisehd = True
+                if interactive == True:
+                    return self.prog
+                else:
+                    if debug: print(output)
+                    return output
+            modes = val//100
+            mod= []
+            while (modes > 0):
+                tmp = modes%10
+                if tmp not in [0, 1]: raise TypeError
+                mod.append(tmp)
+                modes = modes//10
+            vars = []
+            for i in range(numVar):
+                try:
+                    m = mod[i]
+                except IndexError:
+                    m = 0
+                if m == 0:
+                    vars.append(self.prog[self.prog[self.idx+1+i]])
+                elif m == 1:
+                    vars.append(self.prog[self.idx+1+i])
+                else: 
+                    raise RuntimeError
+            if writes:
+                # an opcode that writes to last parameter
+                if op == INP and interactive == False:
+                    if self.inp.empty():
+                        #print("empty")
+                        return output
+                    self.prog[self.prog[self.idx+numVar]] = op(vars[:-1], interactive=False, value=self.inp.get(block=False))
+                else:
+                    self.prog[self.prog[self.idx+numVar]] = op(vars[:-1])
+            elif jumps:
+                #print("JUMP")
+                if op(vars[:-1]):
+                    self.idx = vars[-1]
+                    continue
+            else:
+                if op == OUT and interactive == False:
+                    output.append(op(vars, interactive=False))
+                    if debug: print(output)
+                    #self.halted = True
+                    self.idx += 0
+                    #return output[-1] 
+                else:
+                    op(vars)
+            ignore = numVar
+            self.idx += 1
+
+
+def runAmps(prog, phases):
+    # initialize amps
+    amp1 = Amp(prog)
+    amp2 = Amp(prog)
+    amp3 = Amp(prog)
+    amp4 = Amp(prog)
+    amp5 = Amp(prog)
+    #print(phases)
+    amp1.inp.put(phases[0])
+    amp2.inp.put(phases[1])
+    amp3.inp.put(phases[2])
+    amp4.inp.put(phases[3])
+    amp5.inp.put(phases[4])
+
+    # provide start command
+    amp1.inp.put(0)
+
+    loops = 0
+    while (not amp5.finisehd):
+        #print("loop: {} amp: 1".format(loops))
+        out1 = amp1.run(debug=False)
+        #print(out1)
+        amp2.inp.put(out1[-1])
+        #print("loop: {} amp: 2".format(loops))
+        out2 = amp2.run(debug=False)
+        amp3.inp.put(out2[-1])
+        #print("loop: {} amp: 3".format(loops))
+        out3 = amp3.run(debug=False)
+        amp4.inp.put(out3[-1])
+        #print("loop: {} amp: 4".format(loops))
+        out4 = amp4.run(debug=False)
+        amp5.inp.put(out4[-1])
+        #print("loop: {} amp: 5".format(loops))
+        out5 = amp5.run(debug=False)
+        amp1.inp.put(out5[-1])
+        loops += 1
+    return out5
+
+
+def optimize2(prog):
+    maxThrust = 0
+    phaseSeq = []
+    resultList = []
+    for perm in list(itertools.permutations([5, 6, 7, 8, 9])): 
+        out = runAmps(prog, perm)
+        resultList.append((perm, out[-1]))
+    #print(resultList)
+    bla = max(resultList, key=lambda x: x[1])
+    #print(bla)
+    phaseSeq, maxThrust = bla
+    print(maxThrust)
+    print(phaseSeq)
+    return maxThrust, list(phaseSeq) 
+        
+
+
+def runPartOne():
+    #optimize([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
+    prog = loadintCode('input_day7')
+    optimize(prog)
+    
+def runPartTwo():
+    #optimize([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
+    prog = loadintCode('input_day7')
+    optimize2(prog)
+    
+def run_small_test():
+    # _in = 7
+    # _exp = 999
+    # _prog = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+    # amp = Amp(_prog)
+    # amp.inp.put(_in)
+    # _out = amp.run( debug=True)
+    # print(_out)
+    # print()
+    print("*************")
+    print()
+    progD = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+    maxThrust, phaseSeq = optimize2(progD)
+
+
+if __name__ == '__main__':
+    runPartOne()
+    #run_small_test()
+    runPartTwo()
---
+++ Day9/run_tests.py
@@ -0,0 +1,374 @@
+import day9
+import unittest
+import io
+import re
+
+from unittest.mock import patch
+
+progA = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
+progB = [3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0]
+progC = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
+progD = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+progE = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
+
+
+class TestRunner(unittest.TestCase):
+    
+    def test_day2_1_1(self):
+        self.assertEqual(day9.runCode([1,9,10,3,2,3,11,0,99,30,40,50]), [3500,9,10,70,2,3,11,0,99,30,40,50])
+
+    def test_day2_1_2(self):
+        self.assertEqual(day9.runCode([1,0,0,0,99]), [2,0,0,0,99])
+    
+    def test_day2_1_3(self):
+        self.assertEqual(day9.runCode([2,3,0,3,99]), [2,3,0,6,99])
+
+    def test_day2_1_4(self):
+        self.assertEqual(day9.runCode([2,4,4,5,99,0]), [2,4,4,5,99,9801])
+
+    def test_day2_1_5(self):
+        self.assertEqual(day9.runCode([1,1,1,4,99,5,6,0,99]), [30,1,1,4,2,5,6,0,99])
+
+    def test_day5_1_1(self):
+        self.assertEqual(day9.runCode([1,9,10,3,2,3,11,0,99,30,40,50]), [3500,9,10,70,2,3,11,0,99,30,40,50])
+
+    def test_day5_1_2(self):
+        self.assertEqual(day9.runCode([1,0,0,0,99]), [2,0,0,0,99])
+    
+    def test_day5_1_3(self):
+        self.assertEqual(day9.runCode([2,3,0,3,99]), [2,3,0,6,99])
+
+    def test_day5_1_4(self):
+        self.assertEqual(day9.runCode([2,4,4,5,99,0]), [2,4,4,5,99,9801])
+
+    def test_day5_1_5(self):
+        self.assertEqual(day9.runCode([1,1,1,4,99,5,6,0,99]), [30,1,1,4,2,5,6,0,99])
+
+    def test_day5_1_6(self):
+        self.assertEqual(day9.runCode([1002,4,3,4,33]), [1002,4,3,4,99])
+
+    def test_day5_1_7(self):
+        self.assertEqual(day9.runCode([1101,100,-1,4,0]), [1101,100,-1,4,99])
+
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_inp_1(self, input, mock_stdout):
+        input.return_value = input_data = "123"
+
+        day9.runCode([3,0,4,0,99])
+
+        #print(mock_stdout.getvalue())
+        regex = r"Output is: (\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+
+        self.assertEqual(matches[-1], input_data)
+
+    def getLastOutput(self, string, nothing):
+        regex = r"Output is: (\d+)"
+        matches = re.findall(regex, string, re.MULTILINE)
+        return matches[-1]
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_inp_2(self, input, mock_stdout):
+        input.return_value = input_data = "-456"
+        
+        day9.runCode([3,0,4,0,99])
+
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+
+        self.assertEqual(matches[-1], input_data)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_eql_1(self, input, mock_stdout):
+        input.return_value = "8"
+        expected = "1"
+
+        day9.runCode([3,9,8,9,10,9,4,9,99,-1,8])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_eql_2(self, input, mock_stdout):
+        input.return_value = "7"
+        expected = "0"
+
+        day9.runCode([3,9,8,9,10,9,4,9,99,-1,8])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_eql_3(self, input, mock_stdout):
+        input.return_value = "-9"
+        expected = "0"
+
+        day9.runCode([3,9,8,9,10,9,4,9,99,-1,8])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+# immediate mode
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_eql_4(self, input, mock_stdout):
+        input.return_value = "8"
+        expected = "1"
+
+        day9.runCode([3,3,1108,-1,8,3,4,3,99])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_eql_5(self, input, mock_stdout):
+        input.return_value = "7"
+        expected = "0"
+
+        day9.runCode([3,3,1108,-1,8,3,4,3,99])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_eql_6(self, input, mock_stdout):
+        input.return_value = "-9"
+        expected = "0"
+
+        day9.runCode([3,3,1108,-1,8,3,4,3,99])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_les_1(self, input, mock_stdout):
+        input.return_value = "7"
+        expected = "1"
+
+        day9.runCode([3,9,7,9,10,9,4,9,99,-1,8])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_les_2(self, input, mock_stdout):
+        input.return_value = "8"
+        expected = "0"
+
+        day9.runCode([3,9,7,9,10,9,4,9,99,-1,8])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_les_3(self, input, mock_stdout):
+        input.return_value = "9"
+        expected = "0"
+
+        day9.runCode([3,9,7,9,10,9,4,9,99,-1,8])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+# immediate mode 
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_les_4(self, input, mock_stdout):
+        input.return_value =  "7"
+        expected = "1"
+
+        day9.runCode([3,3,1107,-1,8,3,4,3,99])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_les_5(self, input, mock_stdout):
+        input.return_value = "8"
+        expected = "0"
+
+        day9.runCode([3,3,1107,-1,8,3,4,3,99])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_les_6(self, input, mock_stdout):
+        input.return_value = "9"
+        expected = "0"
+
+        day9.runCode([3,3,1107,-1,8,3,4,3,99])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_jmp_1(self, input, mock_stdout):
+        input.return_value = "0"
+        expected = "0"
+
+        day9.runCode([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_jmp_2(self, input, mock_stdout):
+        input.return_value = "-100"
+        expected = "1"
+
+        day9.runCode([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_jmp_3(self, input, mock_stdout):
+        input.return_value = "123"
+        expected = "1"
+
+        day9.runCode([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    # immediate mode
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_jmp_4(self, input, mock_stdout):
+        input.return_value = "0"
+        expected = "0"
+
+        day9.runCode([3,3,1105,-1,9,1101,0,0,12,4,12,99,1])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_jmp_5(self, input, mock_stdout):
+        input.return_value = "-100"
+        expected = "1"
+
+        day9.runCode([3,3,1105,-1,9,1101,0,0,12,4,12,99,1])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_jmp_6(self, input, mock_stdout):
+        input.return_value = "123"
+        expected = "1"
+
+        day9.runCode([3,3,1105,-1,9,1101,0,0,12,4,12,99,1])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    # test complex
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_run_1(self, input, mock_stdout):
+        input.return_value = "7"
+        expected = "999"
+
+        day9.runCode([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_run_2(self, input, mock_stdout):
+        input.return_value = "8"
+        expected = "1000"
+
+        day9.runCode([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch.object(day9, "input", create=True)
+    def test_run_3(self, input, mock_stdout):
+        input.return_value = "9"
+        expected = "1001"
+
+        day9.runCode([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99])
+        regex = r"Output is: (-*\d+)"
+        matches = re.findall(regex, mock_stdout.getvalue(), re.MULTILINE)
+        self.assertEqual(matches[-1], expected)
+
+    def test_z_day7_1_1(self):
+        maxThrust, phaseSeq = day9.optimize(progA)
+        self.assertEqual(maxThrust, 43210)
+        self.assertEqual(phaseSeq, [4,3,2,1,0])
+    
+    def test_z_day7_1_2(self):
+        maxThrust, phaseSeq = day9.optimize(progB)
+        self.assertEqual(maxThrust, 54321)
+        self.assertEqual(phaseSeq, [0,1,2,3,4])
+
+    def test_z_day7_1_3(self):
+        maxThrust, phaseSeq = day9.optimize(progC)
+        self.assertEqual(maxThrust, 65210)
+        self.assertEqual(phaseSeq, [1,0,4,3,2])
+
+    def test_zz_run_1(self):
+        _in = 7
+        _exp = 999
+        _prog = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+        amp = day9.Amp(_prog)
+        amp.inp.put(_in)
+        _out = amp.run(debug=False)[-1]
+        self.assertEqual(_out, _exp)
+    
+    def test_zz_run_2(self):
+        _in = 8
+        _exp = 1000
+        _prog = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+        amp = day9.Amp(_prog)
+        amp.inp.put(_in)
+        _out = amp.run(debug=False)[-1]
+        self.assertEqual(_out, _exp)
+
+    def test_zz_run_3(self):
+        _in = 9
+        _exp = 1001
+        _prog = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+        amp = day9.Amp(_prog)
+        amp.inp.put(_in)
+        _out = amp.run(debug=False)[-1]
+        self.assertEqual(_out, _exp)
+    
+    def test_zzz_day7_2_1(self):
+        maxThrust, phaseSeq = day9.optimize2(progD)
+        self.assertEqual(maxThrust, 139629729)
+        self.assertEqual(phaseSeq, [9,8,7,6,5])
+
+    def test_zzz_day7_2_2(self):
+        maxThrust, phaseSeq = day9.optimize2(progE)
+        self.assertEqual(maxThrust, 18216)
+        self.assertEqual(phaseSeq, [9,7,8,5,6])
+
+
+if __name__ == '__main__':
+    unittest.main()
