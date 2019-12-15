dd035787421b50ecd8958f9a71f97b9c4b882316
Lukas <lukasHD@users.noreply.github.com>
2019-12-15 18:40:46 +0100

    day8 skeleton

---
+++ Day8/day8.py
@@ -0,0 +1,8 @@
+# day8
+
+
+
+if __name__ == '__main__':
+    runPartOne()
+    #run_small_test()
+    runPartTwo()
---
+++ Day8/run_tests.py
@@ -0,0 +1,13 @@
+import day8
+import unittest
+
+
+
+class TestRunner(unittest.TestCase):
+    
+    def test_day8_1_1(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
