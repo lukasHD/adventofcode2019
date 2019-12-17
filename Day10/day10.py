7fdace04276dae769f0dc24488967a52285c4920
Lukas <lukasHD@users.noreply.github.com>
2019-12-17 23:15:03 +0100

    day 10 skeleton

---
+++ Day10/day10.py
@@ -0,0 +1,11 @@
+# --- Day 10: Monitoring Station ---
+
+
+def run_small_test():
+    print("small test")
+    return 0
+
+if __name__ == '__main__':
+    #runPartOne()
+    run_small_test()
+    #runPartTwo()
---
+++ Day10/run_tests.py
@@ -0,0 +1,10 @@
+import day10
+import unittest
+
+class TestRunner(unittest.TestCase):
+    
+    def test_day10_1_1(self):
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
