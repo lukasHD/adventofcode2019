83a39347a76dcbe9594faf4cbbcbfdff2430756f
Lukas <lukasHD@users.noreply.github.com>
2019-12-23 19:42:25 +0100

    finished day10

--- Day10/day10.py
+++ Day10/day10.py
@@ -1,6 +1,7 @@
 # --- Day 10: Monitoring Station ---
 
-from math import gcd
+from math import gcd, sqrt, pow, atan, atan2, degrees
+from itertools import groupby, cycle
 
 def read_file(fname):
     with open(fname) as f:
@@ -28,6 +29,14 @@ def get_astroids(inStr):
     #print(coordinates)
     return coordinates
 
+def get_station(inStr): 
+    for lNr, line in enumerate(inStr.split('\n')):
+        for chNr, char in enumerate(line): 
+            if char == 'X':
+                # is station
+                station = (chNr, lNr)
+    return station
+
 def get_direction(p1, p2):
     # return the direction from p1 to p2 
     dx = p2[0] - p1[0]
@@ -39,6 +48,23 @@ def get_direction(p1, p2):
         return None
     return (int(dx), int(dy))
 
+def get_dist(p1, p2):
+    # return the direction from p1 to p2 
+    dx = p2[0] - p1[0]
+    dy = p2[1] - p1[1]      
+    dist = sqrt(pow(dx, 2) + pow(dy, 2))
+    return dist
+
+def get_angle(p1, p2):
+    dx = p2[0] - p1[0]
+    dy = p2[1] - p1[1]
+    # if dx == 0 and dy > 0:
+    #     return 9999
+    # if dx == 0 and dy < 0:
+    #     return 2222
+    return (180-degrees(atan2(dx, dy))) % 360
+
+
 def get_number_visible(base, coordinates):
     directions = set()
     for target in coordinates:
@@ -46,6 +72,14 @@ def get_number_visible(base, coordinates):
             directions.add(direction)
     #print(directions)
     return len(directions)
+
+def get_direction_distance(base, coordinates):
+    results = list()
+    for target in coordinates:
+        if (distance  := get_dist(base, target)) != 0:
+            angle      = get_angle(base, target)
+            results.append([target, distance, angle])
+    return results
 
 def find_best_station(coordinates):
     scan = []
@@ -66,8 +100,76 @@ def run_small_test():
     # print(get_direction((1,2),(0,0)))
     # print(get_number_visible((0,1), coordinates))
     find_best_station(coordinates)
-
     return 0
+
+def shoot(station, asteroids): 
+    results = get_direction_distance(station, asteroids)
+    results.sort(key= lambda x: (x[2], x[1]))
+    # is sorted by angle, then by distance
+    print(results)
+    old_angle = -1
+    counter = 1
+    idx = 0 
+    shooting_list = []
+    while len(results) > 0:
+        print("pos {} of {} old_angle = {}".format(idx, len(results), old_angle))
+        i = results[idx]
+        if i[2] == old_angle:
+            print("charge Laser and continue")
+            idx += 1 
+            if idx > (len(results)-1):
+                print("wrap-around inplace")
+                # wrap around and forget all charging
+                if len(results) > 1:
+                    idx = idx % (len(results)-1)
+                else:
+                    idx = 0
+                old_angle = -1
+            continue
+        print("Shot #{} at {}".format(counter, i[0]))
+        shooting_list.append(i[0])
+        old_angle = i[2]
+        counter += 1
+        results.remove(i)
+        if len(results) == 0: 
+            print("finished")
+            break
+        if idx > (len(results)-1):
+            print("wrap-around iterator")
+            # wrap around and forget all charging
+            idx = idx % (len(results)-1)
+            old_angle = -1
+    return shooting_list
+
+def run_small_test2():
+    print("small test 2")
+    print("************")
+    inp = """.#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....X...###..
+..#.#.....#....##"""
+    print(inp)
+    station = get_station(inp)
+    asteroids = get_astroids(inp)
+    print(station)
+    print(asteroids)
+
+    history_of_targets = shoot(station, asteroids)
+    print(history_of_targets)
+
+
+def runPartTwo():
+    inp = read_file2('input')
+    station, numVisible = find_best_station(get_astroids(inp))
+    asteroids = get_astroids(inp)
+    print(station)
+    print(asteroids)
+    history_of_targets = shoot(station, asteroids)
+    print(history_of_targets)
+    twohundred = history_of_targets[200-1]
+    solution = 100*twohundred[0] + twohundred[1]
+    print(solution)
 
 def runPartOne():
     inp = read_file2('input')
@@ -77,6 +179,7 @@ def runPartOne():
     return numVisible
 
 if __name__ == '__main__':
-    runPartOne()
     #run_small_test()
-    #runPartTwo()
+    runPartOne()
+    #run_small_test2()
+    runPartTwo()
--- Day10/run_tests.py
+++ Day10/run_tests.py
@@ -91,5 +91,37 @@ class TestRunner(unittest.TestCase):
         self.assertEqual(bestLocation, (11,13))
         self.assertEqual(numVisible, 210)
 
+    def test_angle(self):
+        self.assertEqual(day10.get_angle((0,0),(0,1)), 180)
+        self.assertEqual(day10.get_angle((0,0),(1,1)), 135)
+        self.assertEqual(day10.get_angle((0,0),(1,0)), 90)
+        self.assertEqual(day10.get_angle((0,0),(1,-1)), 45)
+        self.assertEqual(day10.get_angle((0,0),(0,-1)), 0)
+        self.assertEqual(day10.get_angle((0,0),(-1,-1)), 315)
+        self.assertEqual(day10.get_angle((0,0),(-1,0)), 270)
+        self.assertEqual(day10.get_angle((0,0),(-1,1)), 225)
+
+    def test_day10_2_1(self):
+        bestLocation, numVisible = day10.find_best_station(day10.get_astroids(inpE))
+        self.assertEqual(bestLocation, (11,13))
+        self.assertEqual(numVisible, 210)
+        shoot_list = day10.shoot(bestLocation, day10.get_astroids(inpE))
+        self.assertEqual(shoot_list[0], (11,12))
+        self.assertEqual(shoot_list[1], (12,1))
+        self.assertEqual(shoot_list[2], (12,2))
+
+        self.assertEqual(shoot_list[10-1], (12,8))
+        self.assertEqual(shoot_list[20-1], (16,0))
+        self.assertEqual(shoot_list[50-1], (16,9))
+        self.assertEqual(shoot_list[100-1], (10,16))
+
+        self.assertEqual(shoot_list[199-1], (9,6))
+        self.assertEqual(shoot_list[200-1], (8,2))
+        self.assertEqual(shoot_list[201-1], (10,9))
+
+        self.assertEqual(shoot_list[299-1], (11,1))
+
+
+
 if __name__ == '__main__':
     unittest.main()
