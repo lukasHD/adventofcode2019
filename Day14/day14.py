1f2e0b55a7987c99579fceb3c55bdc589e0a287c
Lukas <lukasHD@users.noreply.github.com>
2019-12-27 23:54:47 +0100

    day14 part1

--- Day14/day14.py
+++ Day14/day14.py
@@ -14,24 +14,25 @@ class Resources:
         for key, values in self.reactions.items():
             print("{} made from {}".format(key,values))
 
-    def produce(self, target, quantity, rest):
+    def produce(self, target, quantity, rest, depth=0):
         if rest == None:
             rest = defaultdict(int)
 
+        intendation = '____'*depth
 
         for key in self.reactions:
             if key[1] == target:
-                print("want to produce {} {} and already have {} {}".format(quantity, target, rest[target], target))
-                print("can do {} {} using: {} ".format(key[0], key[1], self.reactions[key]))
+                print("{}want to produce {} {} and already have {} {}".format(intendation, quantity, target, rest[target], target))
+                print("{}can do {} {} using: {} ".format(intendation, key[0], key[1], self.reactions[key]))
                 
                 if not (quantity - rest[target]) % key[0]:
                     rest[target] = 0
                     a = (quantity - rest[target]) // key[0]
-                    print("glatt rest[{}]=0  a = {}".format(target, a))
+                    print("{}glatt rest[{}]=0  a = {}".format(intendation, target, a))
                 else:
                     a = ((quantity - rest[target]) // key[0]) + 1
                     rest[target] = key[0] - ((quantity - rest[target]) % key[0])
-                    print("krumm rest[{}]={}  a = {}".format(target, rest[target], a))
+                    print("{}krumm rest[{}]={}  a = {}".format(intendation, target, rest[target], a))
 
                 #print(a)
                 if a == 0:
@@ -40,14 +41,16 @@ class Resources:
                 # if quantity % key[0] != 0:
                 #     multiple += 1
                 # overproduced = multiple * key[0] - quantity
-                print("Therefore produce '{} {}' {} times and keep {} pieces".format(key[0], key[1], a, rest[target]))
+                
+                print("{}Therefore produce '{} {}' {} times and keep {} pieces".format(intendation, key[0], key[1], a, rest[target]))
                 #rest[key[1]] += overproduced
                 cost = 0
                 #depth = 0
                 for source in self.reactions[key]:
                     if source[1] == "ORE":
-                        return source[0], rest
-                    bla, rest = self.produce(source[1], source[0] * a, rest)
+                        print("{}Found {} ORE".format(intendation, source[0]*a))
+                        return source[0]*a, rest
+                    bla, rest = self.produce(source[1], source[0] * a, rest, depth=depth+1)
                     #print(bla)
                     cost += bla
                 return cost, rest
@@ -58,13 +61,13 @@ def load(string):
     #print(string)
     resourceTree = Resources()
     for line in string.split('\n'):
-        print(line)
+        #print(line)
         reaction = line.split(' => ')
-        print(reaction)
+        #print(reaction)
         needed = reaction[0].split(', ')
         sources = [(int(x.split()[0]), x.split()[1]) for x in needed]
         product = tuple((int(reaction[1].strip().split(' ')[0]), reaction[1].strip().split(' ')[1]))
-        print("generate {} from {}".format(product, sources))
+        #print("generate {} from {}".format(product, sources))
         resourceTree.add(product, sources)
     resourceTree.showReactions()
     return resourceTree
@@ -91,6 +94,11 @@ def run_small_test():
 def runPartOne():
     print("run Part One")
     print("############")
+    with open('input', 'r') as f:
+        inp = f.read()
+    myTree = load(inp)
+    aa = myTree.produce("FUEL", 1, None)
+    print(aa)
 
 def run_small_test2():
     print("small Test 2")
