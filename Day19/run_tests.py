50b296c706138be743f97fe5e9eafe769e1e89e4
Lukas <lukasHD@users.noreply.github.com>
2019-12-30 00:15:23 +0100

    add day19

---
+++ Day19/run_tests.py
@@ -0,0 +1,10 @@
+import day19
+import unittest
+
+class TestRunner(unittest.TestCase):
+    
+    def test_day11_1_1(self):
+        self.assertEqual(8, 8)
+
+if __name__ == '__main__':
+    unittest.main()
