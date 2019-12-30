b80cc0f64045fee67d5dab6e0cf5175a2cd039e2
Lukas <lukasHD@users.noreply.github.com>
2019-12-30 02:19:37 +0100

    add all skeletons

---
+++ Day23/day23.py
@@ -0,0 +1,25 @@
+# 
+
+
+def run_small_test():
+    print("small Test 1")
+    print("############")
+
+def runPartOne():
+    print("run Part One")
+    print("############")
+
+def run_small_test2():
+    print("small Test 2")
+    print("############")
+
+def runPartTwo():
+    print("run Part Two")
+    print("############")
+
+
+if __name__ == '__main__':
+    run_small_test()
+    runPartOne()
+    run_small_test2()
+    runPartTwo()
