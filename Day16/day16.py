ee6a9c2d533f891bb1a629a5aa58c257d9e97a9b
Lukas <lukasHD@users.noreply.github.com>
2020-01-06 19:24:09 +0100

    day16 part1

--- Day16/day16.py
+++ Day16/day16.py
@@ -1,13 +1,60 @@
-# 
+# --- Day 16: Flawed Frequency Transmission ---
+
+def int2list(inNumber):
+    return list(map(int, list(inNumber)))
+
+def phase(Inlist, debug=True):
+    basePattern = [0, 1, 0, -1]
+    outNumber = []
+    for outDigit in range(len(Inlist)):
+        applyPattern = []
+        for i in basePattern:
+            for _ in range(outDigit+1):
+                applyPattern.append(i)
+        #print("outDigit {}  ---  applypattern {}".format(outDigit, applyPattern))
+        digit = 0
+        for i, el in enumerate(Inlist):
+            if debug: print("{}*{:2d} + ".format(el, applyPattern[(i+1)%len(applyPattern)]), end='')
+            digit += (int(el)*applyPattern[(i+1)%len(applyPattern)])
+        realdigit = int(list(str(digit))[-1])
+        if debug: print(" = {:3d}  ==> realdigit {}".format(digit, realdigit))
+        outNumber.append(realdigit)
+    if debug: print("outnumber = {}".format(outNumber))
+    return outNumber
+
+
+def loadInput(fname='input'):
+    with open(fname, 'r') as f:
+        l = f.read()
+    print(l)
+    return l
+
+def hundertPhases(inlist):
+    out = ''.join(map(str, phase(inlist, debug=False)))
+    print('.', end='')
+    for _ in range(99):
+        out = ''.join(map(str, phase(out, debug=False)))
+        print('.', end='')
+    outList = list(out)[0:8]
+    return outList
 
 
 def run_small_test():
     print("small Test 1")
     print("############")
+    aaa = int2list('12345678')
+    print(aaa)
+    phase(aaa)
+    hundertPhases(aaa)
 
 def runPartOne():
     print("run Part One")
     print("############")
+    Inlist = loadInput()
+    out2 = hundertPhases(Inlist)
+    out = ''.join(map(str, out2))
+    print(out)
+
 
 def run_small_test2():
     print("small Test 2")
--- Day16/run_tests.py
+++ Day16/run_tests.py
@@ -1,10 +1,55 @@
 import day16
 import unittest
 
+
+# 80871224585914546619083218645595 becomes 24176176.
+# 19617804207202209144916044189917 becomes 73745418.
+# 69317163492948606335995924319873 becomes 52432133.
+
+
 class TestRunner(unittest.TestCase):
     
-    def test_day11_1_1(self):
-        self.assertEqual(8, 8)
+    def test_day16_1_1(self):
+        Input = '80871224585914546619083218645595'
+        expected = '24176176'
+        out2 = day16.hundertPhases(day16.int2list(Input))
+        out = ''.join(map(str, out2))
+        self.assertEqual(out, expected)
+
+    def test_day16_1_2(self):
+        Input = '19617804207202209144916044189917'
+        expected = '73745418'
+        out2 = day16.hundertPhases(day16.int2list(Input))
+        out = ''.join(map(str, out2))
+        self.assertEqual(out, expected)
+
+    def test_day16_1_3(self):
+        Input = '69317163492948606335995924319873'
+        expected = '52432133'
+        out2 = day16.hundertPhases(day16.int2list(Input))
+        out = ''.join(map(str, out2))
+        self.assertEqual(out, expected)
+
+    def test_day16_1_4(self):
+        Input = '12345678'
+
+        expected = '48226158'
+        out2 = day16.phase(day16.int2list(Input))
+        out = ''.join(map(str, out2))
+        self.assertEqual(out, expected)
+        
+        expected = '34040438'
+        out2 = day16.phase(day16.int2list(out))
+        out = ''.join(map(str, out2))
+
+        expected ='03415518'
+        out2 = day16.phase(day16.int2list(out))
+        out = ''.join(map(str, out2))
+
+        expected = '01029498'
+        out2 = day16.phase(day16.int2list(out))
+        out = ''.join(map(str, out2))
+
 
 if __name__ == '__main__':
     unittest.main()
