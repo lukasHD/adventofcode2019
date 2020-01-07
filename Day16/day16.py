5bac62823b63b22454326e5177cafc221e9924f8
Lukas <lukasHD@users.noreply.github.com>
2020-01-07 20:31:41 +0100

    finisehd day 16

--- Day16/day16.py
+++ Day16/day16.py
@@ -6,15 +6,26 @@ import cProfile
 def int2list(inNumber):
     return list(map(int, list(inNumber)))
 
-def phase(Inlist, debug=True):
+def phase(Inlist, debug=True, ultra=False):
     basePattern = [0, 1, 0, -1]
     outNumber = []
-    for outDigit in range(len(Inlist)):
+    inlen = len(Inlist)
+    for outDigit in range(inlen):
         applyPattern = []
         for i in basePattern:
             for _ in range(outDigit+1):
                 applyPattern.append(i)
         #print("outDigit {}  ---  applypattern {}".format(outDigit, applyPattern))
+        if ultra:
+            test = ""
+            for i in applyPattern:
+                if i == 1:
+                    test += '+'
+                elif i == 0:
+                    test += '0'
+                elif i == -1:
+                    test += '-'
+            print("outDigit {:3d}  ---  applypattern {}".format(outDigit, test[:inlen]))
         digit = 0
         length = len(applyPattern)
         for i, el in enumerate(Inlist):
@@ -52,10 +63,16 @@ def hundertPhases(inlist):
 def run_small_test():
     print("small Test 1")
     print("############")
-    aaa = int2list('12345678')
+    aaa = int2list('12345678'*10)
     print(aaa)
-    phase(aaa)
-    hundertPhases(aaa)
+    phase(aaa, debug=False, ultra=True)
+
+    Inlist = loadInput()
+    print(len(Inlist))
+    Inlist = loadInput()
+    print(len(Inlist*10000))
+    print("{}/{} = {}".format(5972883, len(Inlist*10000), 5972883/len(Inlist*10000)))
+    #hundertPhases(aaa)
 
 def runPartOne():
     print("run Part One")
@@ -92,9 +109,27 @@ def runPartTwo():
     print("run Part Two")
     print("############")
 
+    # https://github.com/Dementophobia/advent-of-code-2019/blob/master/2019_16_p2.py 
+    def solve():
+        with open('input') as f:
+            content = f.readlines()
+        input = [x.strip() for x in content][0]
+
+        #input    = read_file("16")[0]
+        offset   = int(input[:7])
+        elements = [int(num) for _ in range(10000) for num in input][offset:]
+        
+        for _ in range(100):
+            for i in range(-2, -len(elements)-1, -1):
+                elements[i] = (elements[i] + elements[i+1]) % 10
+
+        return "".join([str(x) for x in elements[:8]])
+
+    result = solve()
+    print(f"Solution: {result}")
 
 if __name__ == '__main__':
-    #run_small_test()
-    runPartOne()
+    run_small_test()
+    #runPartOne()
     #run_small_test2()
     runPartTwo()
